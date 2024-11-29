8d60b428e653b1f2552db663805c0d62a8ef0a8f
Konstantin Mihaylov <konstantin.mihaylov.io@gmail.com>
2024-11-29 13:02:36 +0100

    Implement avatar generator

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,31 @@ const Home = () => {
     return (
         <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
             <main className='flex flex-col gap-8 row-start-2 items-center sm:items-start'>
+                <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='150'
+                    height='150'
+                >
+                    <defs>
+                        <linearGradient
+                            id='gradient'
+                            x1='0'
+                            y1='0'
+                            x2='1'
+                            y2='1'
+                        >
+                            <stop offset='0%' stop-color='#3d5ff5' />
+                            <stop offset='100%' stop-color='#f53d5f' />
+                        </linearGradient>
+                    </defs>
+                    <rect
+                        width='150'
+                        height='150'
+                        rx='0'
+                        ry='0'
+                        fill='url(#gradient)'
+                    />
+                </svg>
                 <Image
                     className='dark:invert'
                     src='/next.svg'
